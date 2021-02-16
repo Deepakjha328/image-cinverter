19d67587a295b45e496660d704a73b85a0def53b
Deepakjha328 <deepakkumar86561@gmail.com>
2021-02-16 17:13:47 +0530

    conveting image jpg to png

---
+++ image.py
@@ -0,0 +1,17 @@
+#from PIL import Image
+
+#im = Image.open("./pokeg/pikachu.jpg")
+#print(im.format)
+#print(im.size)
+#print(im.mode)
+#print(dir(im))
+
+#img_filter = im.filter(ImageFilter.BLUR)
+#img_filter =im.convert('L')
+#box = (100, 100, 400, 400)
+#region = img_filter.crop(box)
+#region.save('grey.png','png')
+#img=Image.open("./pokeg/car.jpg")
+#img.thumbnail((500,800))
+#mg.save('thumbnail.jpg')
+#print(img.size)
